57d74fb70d012d3b3c5743b031b1d43b8d3052e8
aggarwal07 <shivamaggarwal179@gmail.com>
2025-03-13 02:18:58 +0530

    home page layouting done

--- src/components/Landing/GamesBanner.js
+++ src/components/Landing/GamesBanner.js
@@ -3,30 +3,42 @@ import OnlineDot from "@/utilities/OnlineDot";
 import Image from "next/image";
 import React from "react";
 
-const GamesBanner = ({ gameData }) => {
+const GamesBanner = ({ gameData, landing = false }) => {
   return (
-    <div className="ml-14 mt-20">
+    <div className="w-fit">
       <div className="w-fit">
-        <p className="text-[70px] font-wallpoet">{gameData?.gameName}</p>
+        <p
+          className={`text-[70px] ${
+            landing ? "font-wallpoet" : "font-aoboshiOne text-white"
+          }`}
+        >
+          {gameData?.gameName}
+        </p>
         <div className="text-right uppercase mt-1 bg-[#1E1E1E] text-xs py-1 px-2">
           RELEASE DATE : {gameData?.releaseDate}
         </div>
       </div>
-      <div className="mt-8 text-md w-[50%] font-prostoOne">
+      <div className="mt-8 text-md w-[40em] font-prostoOne">
         {gameData?.gameDescription}
       </div>
-      <div className="flex mt-14 gap-7">
+      <div className="flex mt-14 gap-7 w-fit">
         <div className="flex flex-col items-center">
-          <MasterButton text="Try For Free" btnWidth="12em" paddingY="15px" />
+          <MasterButton
+            text={landing ? "Try For Free" : "Play Now"}
+            btnWidth="12em"
+            paddingY="15px"
+          />
           <p className="mt-2">Buy now for {gameData?.gamePrice} only</p>
-          <div className="flex items-center gap-2 mt-7">
-            <OnlineDot />
-            <p className="text-[12px] mb-0">
-              {gameData?.friendsPlaying} of your friends are playing
-            </p>
-          </div>
+          {landing && (
+            <div className="flex items-center gap-2 mt-7">
+              <OnlineDot />
+              <p className="text-[12px] mb-0">
+                {gameData?.friendsPlaying} of your friends are playing
+              </p>
+            </div>
+          )}
         </div>
-        <div className="flex items-center gap-3 h-fit mt-2">
+        <div className="flex items-center gap-3 h-fit mt-2 w-fit">
           <p className="mb-0">Available on :</p>
           <Image
             alt="polaroid"
--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -6,15 +6,21 @@ import { gamesCardData, gamesData } from "@/appConstants/AppConstants";
 import GamesBanner from "./GamesBanner";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import GamesCard from "./GamesCard";
+import OnlineDot from "@/utilities/OnlineDot";
+import { Rating } from "@mui/material";
 
 const LandingPage = () => {
   const slides = gamesData.map((item, index) => ({
-    slide: <GamesBanner key={index} gameData={item} />,
+    slide: (
+      <div className="ml-14 mt-20">
+        <GamesBanner key={index} gameData={item} landing={true} />
+      </div>
+    ),
   }));
   return (
     <div className="w-full relative">
       {/* sidebar starts */}
-      <div className="md:w-[7vw] absolute left-0 top-0 border-r border-[var(--secondary-border-color)] h-[100%]">
+      <div className="md:w-[7vw] absolute left-0 top-0 border-r border-[var(--secondary-border-color)] h-[100%] z-[10]">
         <SideBar />
       </div>
       {/* sidebar ends */}
@@ -27,7 +33,7 @@ const LandingPage = () => {
       {/* landing section ends */}
 
       {/* gameCards section starts */}
-      <div className="md:pl-[7vw] w-full py-14 overflow-x-hidden">
+      <div className="md:pl-[7vw] w-full py-16 overflow-x-hidden">
         <div className="px-5">
           <div className="flex items-center justify-between">
             <p className="text-4xl font-press2p text-[var(--secondary-foreground)]">
@@ -46,6 +52,57 @@ const LandingPage = () => {
         </div>
       </div>
       {/* gameCards section ends */}
+
+      {/*detailed gameCards section starts */}
+      <div className="md:pl-[7vw] w-full bg-[var(--secondary-background)] py-16 flex justify-end pr-36 relative pl-5">
+        <div className="absolute right-8 top-8 flex flex-col items-end">
+          <div className="flex items-center gap-2">
+            <OnlineDot />
+            <p className="text-[12px] mb-0">
+              {gamesData[1]?.friendsPlaying} of your friends are playing
+            </p>
+          </div>
+          <div className="mt-1">
+            <Rating name="read-only" value={3} readOnly />
+          </div>
+        </div>
+        <GamesBanner gameData={gamesData[1]} />
+      </div>
+
+      <div className="md:pl-[7vw] w-full bg-[var(--secondary-background)] py-16 flex justify-center relative mt-14">
+        <div className="px-5">
+          <div className="absolute right-8 top-8 flex flex-col items-end">
+            <div className="flex items-center gap-2">
+              <OnlineDot />
+              <p className="text-[12px] mb-0">
+                {gamesData[2]?.friendsPlaying} of your friends are playing
+              </p>
+            </div>
+            <div className="mt-1">
+              <Rating name="read-only" value={3} readOnly />
+            </div>
+          </div>
+          <GamesBanner gameData={gamesData[2]} />
+        </div>
+      </div>
+
+      <div className="md:pl-[7vw] w-full bg-[var(--secondary-background)] py-16 flex justify-start px-5 relative mt-14">
+        <div className="px-5">
+          <div className="absolute right-8 top-8 flex flex-col items-end">
+            <div className="flex items-center gap-2">
+              <OnlineDot />
+              <p className="text-[12px] mb-0">
+                {gamesData[3]?.friendsPlaying} of your friends are playing
+              </p>
+            </div>
+            <div className="mt-1">
+              <Rating name="read-only" value={3} readOnly />
+            </div>
+          </div>
+          <GamesBanner gameData={gamesData[3]} />
+        </div>
+      </div>
+      {/*detailed gameCards section ends */}
     </div>
   );
 };
