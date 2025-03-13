0e872b7b508cef3fd56055b5c5101f9fb78b0f4d
aggarwal07 <shivamaggarwal179@gmail.com>
2025-03-13 21:16:48 +0530

    pagination added to product listing

--- src/components/Landing/GamesCard.js
+++ src/components/Landing/GamesCard.js
@@ -1,28 +1,42 @@
 "use client";
 import MasterButton from "@/utilities/MasterButton";
+import OfflineDot from "@/utilities/OfflineDot";
 import OnlineDot from "@/utilities/OnlineDot";
 import { Rating } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 const GamesCard = ({ gameCardData }) => {
   return (
-    <div className="rounded-[10px] p-4 bg-[var(--gameCard-bg-color)] h-[23em] flex flex-col justify-between text-black w-[21.5em] min-w-[21.5em]">
-      <div className="rounded-[20px] bg-[var(--secondary-background)] flex items-center py-1 px-2 w-fit">
-        <OnlineDot />
+    <div className="rounded-[10px] p-4 bg-[var(--gameCard-bg-color)] h-[23em] flex flex-col justify-between text-black w-[21.5em] min-w-[21.5em] relative overflow-hidden">
+      <Image
+        alt="polaroid"
+        src={gameCardData?.thumbnail || "/Images/Logos/iosLogo.png"}
+        width={50}
+        height={50}
+        className="absolute w-full h-full left-0 object-cover"
+      />
+      <div className="rounded-[20px] bg-[var(--secondary-background)] flex items-center py-1 px-2 w-fit z-[1]">
+        {gameCardData?.stock !== 0 ? <OnlineDot /> : <OfflineDot />}
         <p className="text-[10px] mb-0 ml-2 text-white">
-          {gameCardData?.playerOnline} Online
+          {gameCardData?.stock} Units Available
         </p>
       </div>
-      <div>
-        <p className="text-3xl font-aoboshiOne">{gameCardData?.gameName}</p>
+      <div className="z-[1]">
+        <p className="text-3xl font-aoboshiOne">{gameCardData?.title}</p>
         <Rating
+          sx={{
+            "& .MuiRating-iconEmpty": {
+              color: "gold",
+            },
+          }}
           name="read-only"
           value={gameCardData?.rating}
           readOnly
           size="small"
         />
         <ul className="flex items-center gap-8 list-disc text-sm text-[#FF1C1C] font-medium">
-          {gameCardData?.highlights?.map((item, index) => {
+          {gameCardData?.tags?.map((item, index) => {
             return (
               <li className={`${index === 0 ? "list-none" : ""}`} key={index}>
                 {item}
@@ -31,12 +45,10 @@ const GamesCard = ({ gameCardData }) => {
           })}
         </ul>
         <p className="text-xs font-medium ">
-          Released {gameCardData?.releasedDate}
+          {gameCardData?.warrantyInformation}
         </p>
         <div className="flex items-center gap-3 mt-2 justify-between">
-          <p className="text-[22px] font-semibold">
-            $ {gameCardData?.gamePrice}
-          </p>
+          <p className="text-[22px] font-semibold">$ {gameCardData?.price}</p>
           <MasterButton
             text="Buy Now"
             btnWidth="12em"
--- src/components/Products/Listing/ProductsFetched.js
+++ src/components/Products/Listing/ProductsFetched.js
@@ -1,14 +1,19 @@
-"use Client";
+"use client";
 import GamesCard from "@/components/Landing/GamesCard";
 import { isLoading } from "@/store/Actions/globalAction";
 import { fetchProducts } from "@/store/Actions/restApiActions";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ProductFilter from "./ProductFilter";
+import Pagination from "@mui/material/Pagination";
 
 const ProductsFetched = () => {
   const [productsList, setProductsList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const dispatch = useDispatch();
   const filterSettings = useSelector((state) => state.globalState.filters);
+
   useEffect(() => {
     dispatch(isLoading(true));
     const pathParam =
@@ -31,15 +36,49 @@ const ProductsFetched = () => {
     filterSettings.upperPriceRange,
   ]);
 
-  console.log(productsList, "productsList");
+  //pagination work starts
+  const itemsPerPage = 12;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentProducts = productsList.slice(indexOfFirstItem, indexOfLastItem);
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+  //pagination work ends
 
   return (
-    <div className="w-fit flex flex-wrap gap-4 justify-start h-fit">
-      {productsList?.map((item, index) => (
-        <div key={index} >
-          <GamesCard/>
+    <div>
+      <div className="flex justify-between">
+        <div>
+          <p className="text-2xl">
+            Search Results for "
+            {filterSettings.categories !== ""
+              ? filterSettings.categories
+              : "All"}
+            "
+          </p>
+          <p className="text-xl">{productsList.length} results found</p>
         </div>
-      ))}
+      </div>
+
+      <div className="w-fit flex flex-wrap gap-4 justify-start h-fit mt-8">
+        {currentProducts?.map((item, index) => (
+          <div key={index}>
+            <GamesCard gameCardData={item} />
+          </div>
+        ))}
+      </div>
+
+      {productsList.length > itemsPerPage && (
+        <div className="flex justify-center mt-6">
+          <Pagination
+            count={Math.ceil(productsList.length / itemsPerPage)}
+            page={currentPage}
+            onChange={handlePageChange}
+            color="primary"
+          />
+        </div>
+      )}
     </div>
   );
 };
